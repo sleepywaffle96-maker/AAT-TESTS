93d71985b43239e17651050f3a50fb6c97e421af
sleepywaffle96-maker <sleepywaffle96@gmail.com>
2026-06-18 22:11:52 +0200

    Update osborne-engine.js

--- osborne-engine.js
+++ osborne-engine.js
@@ -982,7 +982,7 @@ A business has the following transactions:
 
 Calculate Assets, Liabilities and Capital.
         `,
-        solution: {
+        answer: {
             assets,
             liabilities,
             capital
